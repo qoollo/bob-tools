7d1d04a254b1bf4c4e52414f7fa18b93ef47df5a
Иван Дружицкий <idruzhitskiy@mail.ru>
2021-05-18 15:59:12 +0000

    add bob cluster conf deserialization

---
+++ src/BobApi/BobEntities/ClusterConfiguration.cs
@@ -0,0 +1,63 @@
+using System.Collections.Generic;
+using YamlDotNet.Serialization;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace BobApi.BobEntities
+{
+    public class ClusterConfiguration
+    {
+        [YamlMember(Alias = "nodes")]
+        public List<Node> Nodes { get; set; }
+
+        [YamlMember(Alias = "vdisks")]
+        public List<VDisk> VDisks { get; set; }
+
+        public class Node
+        {
+            [YamlMember(Alias = "name")]
+            public string Name { get; set; }
+
+            [YamlMember(Alias = "address")]
+            public string Address { get; set; }
+
+            [YamlMember(Alias = "disks")]
+            public List<Disk> Disks { get; set; }
+
+            public class Disk
+            {
+                [YamlMember(Alias = "name")]
+                public string Name { get; set; }
+
+                [YamlMember(Alias = "path")]
+                public string Path { get; set; }
+            }
+        }
+
+        public class VDisk
+        {
+            [YamlMember(Alias = "id")]
+            public long Id { get; set; }
+
+            [YamlMember(Alias = "replicas")]
+            public List<Replica> Replicas { get; set; }
+
+            public class Replica
+            {
+                [YamlMember(Alias = "node")]
+                public string Node { get; set; }
+
+                [YamlMember(Alias = "disk")]
+                public string Disk { get; set; }
+            }
+        }
+
+        public static async Task<ClusterConfiguration> FromYamlFile(string filename)
+        {
+            var deserializer = new Deserializer();
+            var content = await File.ReadAllTextAsync(filename);
+            return deserializer.Deserialize<ClusterConfiguration>(content);
+        }
+    }
+}
+
