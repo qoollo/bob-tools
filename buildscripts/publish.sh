91e68dff967b10388267e66344a5c616d5c65302
Иван Дружицкий <idruzhitskiy@mail.ru>
2021-06-09 10:26:03 +0000

    return publish lines

--- buildscripts/publish.sh
+++ buildscripts/publish.sh
@@ -3,4 +3,8 @@ VERSION=$(git describe --tags)
 CONFIGURATION=Release
 RUNTIME=linux-x64
 echo $VERSION
+dotnet publish -c $CONFIGURATION -r $RUNTIME -p:Version=$VERSION -o publish src/DisksMonitoring
+dotnet publish -c $CONFIGURATION -r $RUNTIME -p:Version=$VERSION -o publish src/OldPartitionsRemover
+dotnet publish -c $CONFIGURATION -r $RUNTIME -p:Version=$VERSION -o publish src/RecordsCalculator
 dotnet publish -c $CONFIGURATION -r $RUNTIME -p:Version=$VERSION -o publish src/DiskStatusAnalyzer
+dotnet publish -c $CONFIGURATION -r $RUNTIME -p:Version=$VERSION -o publish src/ClusterModifier
