c8efd623d60d4c49983d3b49134826094ba7fb02
Ivan Druzhitskiy <idruzhitskiy@mail.ru>
2021-10-05 16:45:45 +0300

    update api client

--- src/BobApi/BobApiClient.cs
+++ src/BobApi/BobApiClient.cs
@@ -12,144 +12,58 @@ namespace BobApi
 {
     public class BobApiClient : IDisposable
     {
-        private readonly HttpClient client;
+        private readonly HttpClient _client;
 
         public BobApiClient(Uri address)
         {
-            client = new HttpClient
+            _client = new HttpClient
             {
                 BaseAddress = address,
                 Timeout = TimeSpan.FromSeconds(30),
             };
         }
 
-        public async Task<Node?> GetStatus()
-        {
-            try
-            {
-                var response = await client.GetAsync("status");
-                if (response.IsSuccessStatusCode)
-                    return await response.Content.ReadAsStringAsync()
-                        .ContinueWith(t => JsonConvert.DeserializeObject<Node>(t.Result));
-            }
-            catch (HttpRequestException) { }
-            return null;
-        }
+        public async Task<Node?> GetStatus(CancellationToken cancellationToken = default)
+            => await GetJson<Node?>("status", cancellationToken);
 
-        public async Task<List<Node>> GetNodes()
-        {
-            try
-            {
-                var response = await client.GetAsync("nodes");
-                if (response.IsSuccessStatusCode)
-                    return await response.Content.ReadAsStringAsync()
-                        .ContinueWith(t => JsonConvert.DeserializeObject<List<Node>>(t.Result));
-            }
-            catch (HttpRequestException) { }
-            return null;
-        }
+        public async Task<List<Node>> GetNodes(CancellationToken cancellationToken = default)
+            => await GetJson<List<Node>>("nodes", cancellationToken);
 
-        public async Task<List<Disk>> GetDisks()
-        {
-            try
-            {
-                using (var response = await client.GetAsync("disks/list"))
-                {
-                    if (response.IsSuccessStatusCode)
-                        return await response.Content.ReadAsStringAsync().ContinueWith(t => JsonConvert.DeserializeObject<List<Disk>>(t.Result));
-                }
-            }
-            catch (HttpRequestException) { }
-            return null;
-        }
+        public async Task<List<Disk>> GetDisks(CancellationToken cancellationToken = default)
+            => await GetJson<List<Disk>>("disks/list", cancellationToken);
 
-        public async Task<List<Disk>> GetInactiveDisks()
+        public async Task<List<Disk>> GetInactiveDisks(CancellationToken cancellationToken = default)
         {
-            var disks = await GetDisks();
+            var disks = await GetDisks(cancellationToken);
             if (disks != null)
                 disks.RemoveAll(d => d.IsActive);
             return disks;
         }
 
         public async Task<bool> StartDisk(string name, CancellationToken cancellationToken = default)
-        {
-            try
-            {
-                using (var response = await client.PostAsync($"disks/{name}/start", new StringContent(""), cancellationToken))
-                    return response.IsSuccessStatusCode;
-            }
-            catch
-            {
-                return false;
-            }
-        }
+            => await PostIsOk($"disks/{name}/start", cancellationToken);
 
         public async Task<bool> StopDisk(string name, CancellationToken cancellationToken = default)
-        {
-            try
-            {
-                using (var response = await client.PostAsync($"disks/{name}/stop", new StringContent(""), cancellationToken))
-                    return response.IsSuccessStatusCode;
-            }
-            catch
-            {
-                return false;
-            }
-        }
+            => await PostIsOk($"disks/{name}/stop", cancellationToken);
 
         public async Task<bool> RestartDisk(string diskName, CancellationToken cancellationToken = default)
-        {
-            return await StopDisk(diskName, cancellationToken) && await StartDisk(diskName, cancellationToken);
-        }
+            => await StopDisk(diskName, cancellationToken) && await StartDisk(diskName, cancellationToken);
 
         public async Task<List<Directory>> GetDirectories(VDisk vdisk, CancellationToken cancellationToken = default)
-        {
-            var response = await client.GetAsync($"vdisks/{vdisk.Id}/replicas/local/dirs", cancellationToken: cancellationToken);
-            if (response.IsSuccessStatusCode)
-                return await response.Content.ReadAsStringAsync()
-                    .ContinueWith(t => JsonConvert.DeserializeObject<List<Directory>>(t.Result));
-            return null;
-        }
+            => await GetJson<List<Directory>>($"vdisks/{vdisk.Id}/replicas/local/dirs", cancellationToken: cancellationToken);
 
-        public async Task<Directory> GetAlienDirectory()
-        {
-            try
-            {
-                var response = await client.GetAsync("alien/dir");
-                var content = await response.Content.ReadAsStringAsync();
-                return JsonConvert.DeserializeObject<Directory>(content);
-            }
-            catch (HttpRequestException)
-            {
-                return default;
-            }
-        }
+        public async Task<Directory> GetAlienDirectory(CancellationToken cancellationToken = default)
+            => await GetJson<Directory>("alien/dir", cancellationToken);
 
         public async Task<bool> SyncAlienData(CancellationToken cancellationToken = default)
-        {
-            try
-            {
-                var response = await client.PostAsync("alien/sync", new StringContent(""), cancellationToken);
-                return response.IsSuccessStatusCode;
-            }
-            catch (HttpRequestException)
-            {
-                return false;
-            }
-        }
+            => await PostIsOk("alien/sync", cancellationToken);
 
         public async Task<List<VDisk>> GetVDisks(CancellationToken cancellationToken = default)
-        {
-            var response = await client.GetAsync("vdisks", cancellationToken: cancellationToken);
-            if (response.IsSuccessStatusCode)
-                return await response.Content.ReadAsStringAsync()
-                    .ContinueWith(t => JsonConvert.DeserializeObject<List<VDisk>>(t.Result));
-            return null;
-        }
+            => await GetJson<List<VDisk>>("vdisks", cancellationToken);
 
         public async Task<List<string>> GetPartitions(VDisk vDisk)
         {
-            var response = await client.GetAsync($"vdisks/{vDisk.Id}/partitions");
+            var response = await _client.GetAsync($"vdisks/{vDisk.Id}/partitions");
             if (response.IsSuccessStatusCode)
             {
                 return await response.Content.ReadAsStringAsync()
@@ -162,60 +76,66 @@ namespace BobApi
             return null;
         }
 
-        public async Task<string> GetAlienDiskName()
-        {
-            const string SpecialDiskName = "alien_disk";
-            var disks = await GetDisks();
-            if (disks is null)
-                return null;
-            if (disks.Any(d => d.Name == SpecialDiskName))
-                return SpecialDiskName;
-
-            var byName = disks.GroupBy(d => d.Name);
-            var multinameGroup = byName.FirstOrDefault(g => g.Count() > 1);
-            if (multinameGroup != null)
-                return multinameGroup.Key;
-
-            return disks.OrderBy(d => d.Name).FirstOrDefault().Name;
-        }
-
         public async Task DeletePartition(VDisk vDisk, long? partition)
-        {
-            await client.DeleteAsync($"vdisks/{vDisk.Id}/partitions/{partition}");
-        }
+            => await _client.DeleteAsync($"vdisks/{vDisk.Id}/partitions/{partition}");
 
-        public async Task<Partition?> GetPartition(VDisk vDisk, string partition)
-        {
-            var result = await client.GetAsync($"vdisks/{vDisk.Id}/partitions/{partition}");
-            if (!result.IsSuccessStatusCode)
-                return null;
-            var content = await result.Content.ReadAsStringAsync();
-            return JsonConvert.DeserializeObject<Partition>(content);
+        public async Task<Partition?> GetPartition(VDisk vDisk, string partition,
+            CancellationToken cancellationToken = default)
+            => await GetJson<Partition>($"vdisks/{vDisk.Id}/partitions/{partition}", cancellationToken);
 
-        }
 
-        public async Task<long?> CountRecordsOnVDisk(VDisk vDisk)
-        {
-            try
-            {
-                var result = await client.GetAsync($"vdisks/{vDisk.Id}/records/count");
-                if (!result.IsSuccessStatusCode)
-                    return null;
-                return long.Parse(await result.Content.ReadAsStringAsync());
-            }
-            catch (HttpRequestException) { }
-            catch (TaskCanceledException) { }
-            return null;
-        }
+        public async Task<long?> CountRecordsOnVDisk(VDisk vDisk, CancellationToken cancellationToken = default)
+            => await GetJson<long?>($"vdisks/{vDisk.Id}/records/count", cancellationToken);
 
         public void Dispose()
         {
-            client?.Dispose();
+            _client?.Dispose();
         }
 
         public override string ToString()
         {
-            return client.BaseAddress.ToString();
+            return _client.BaseAddress.ToString();
+        }
+
+        private async Task<T> GetJson<T>(string addr, CancellationToken cancellationToken = default)
+            => await Get(addr, JsonConvert.DeserializeObject<T>, cancellationToken);
+
+
+        private async Task<bool> PostIsOk(string addr, CancellationToken cancellationToken = default)
+        {
+            return await InvokeRequest(async client =>
+            {
+                using (var response = await client.PostAsync(addr, new StringContent(""), cancellationToken: cancellationToken))
+                    return response.IsSuccessStatusCode;
+            });
+        }
+
+        private async Task<T> Get<T>(string addr, Func<string, T> parse, CancellationToken cancellationToken = default)
+        {
+            return await InvokeRequest(async client =>
+            {
+                using (var response = await client.GetAsync(addr, cancellationToken))
+                {
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var content = await response.Content.ReadAsStringAsync();
+                        return parse(content);
+                    }
+                    return default;
+                }
+            });
+        }
+
+        private async Task<T> InvokeRequest<T>(Func<HttpClient, Task<T>> f)
+        {
+            try
+            {
+                return await f(_client);
+            }
+            catch (HttpRequestException) { }
+            catch (TaskCanceledException) { } // Since .net 5 it is thrown on timeout
+
+            return default;
         }
     }
 }
