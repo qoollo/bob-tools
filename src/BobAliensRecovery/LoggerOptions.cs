a8010a1dde5684daa8639605fd266e4a623bb4a7
Ivan Druzhitskiy <idruzhitskiy@mail.ru>
2021-10-05 17:18:31 +0300

    move logging init to cons

--- src/BobAliensRecovery/LoggerOptions.cs
+++ src/BobAliensRecovery/LoggerOptions.cs
@@ -6,26 +6,18 @@ namespace BobAliensRecovery
 {
     class LoggerOptions
     {
-        private readonly int _verbosityLevel;
-
         public LoggerOptions(int verbosityLevel)
         {
-            _verbosityLevel = verbosityLevel;
+            MinLevel = verbosityLevel switch
+            {
+                3 => LogLevel.Trace,
+                2 => LogLevel.Debug,
+                1 => LogLevel.Information,
+                0 => LogLevel.Error,
+                _ => throw new ArgumentException("Verbosity must be in range [0; 3]")
+            };
         }
 
-        public LogLevel MinLevel
-        {
-            get
-            {
-                return _verbosityLevel switch
-                {
-                    3 => LogLevel.Trace,
-                    2 => LogLevel.Debug,
-                    1 => LogLevel.Information,
-                    0 => LogLevel.Error,
-                    _ => throw new ArgumentException("Verbosity must be in range [0; 3]")
-                };
-            }
-        }
+        public LogLevel MinLevel { get; }
     }
 }
