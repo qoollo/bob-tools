1f9b24d36f45fe27d1a59c94dfa692e1e4872c9d
Ivan Druzhitskiy <idruzhitskiy@mail.ru>
2022-01-21 15:58:12 +0300

    add support for hmy thresholds

--- src/OldPartitionsRemover/ByDateRemoving/Arguments.cs
+++ src/OldPartitionsRemover/ByDateRemoving/Arguments.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Globalization;
 using System.Text.RegularExpressions;
 using BobToolsCli;
 using CommandLine;
@@ -10,6 +11,7 @@ namespace OldPartitionsRemover.ByDateRemoving
     public class Arguments : CommonArguments
     {
         private static readonly Regex s_daysOffsetRegex = new(@"^\-(\d+)d$");
+        private static readonly Regex s_timeSpanRegex = new(@"^\-(?<span>\d+)(?<unit>[dhmy])");
 
         [Option('t', "threshold", HelpText = "Removal threshold. Can be either date or in relative days count format, e.g. \"-3d\"", Required = true)]
         public string ThresholdString { get; set; }
@@ -18,13 +20,34 @@ namespace OldPartitionsRemover.ByDateRemoving
         {
             if (string.IsNullOrWhiteSpace(ThresholdString))
                 return Result<DateTimeOffset>.Error("Removal threshold not set");
-            if (s_daysOffsetRegex.IsMatch(ThresholdString))
-                return Result<DateTimeOffset>.Ok(DateTime.Now - TimeSpan.FromDays(
-                    int.Parse(s_daysOffsetRegex.Match(ThresholdString).Groups[1].Value)));
+            if (TryParseTimeSpan(ThresholdString, out var ts))
+                return Result<DateTimeOffset>.Ok(DateTime.Now + ts);
             else if (DateTime.TryParse(ThresholdString, out var dateTimeThreshold))
                 return Result<DateTimeOffset>.Ok(dateTimeThreshold);
 
             return Result<DateTimeOffset>.Error("Failed to parse threshold");
+        }
+
+        private static bool TryParseTimeSpan(string s, out TimeSpan ts)
+        {
+            ts = TimeSpan.Zero;
+            var match = s_timeSpanRegex.Match(s);
+            if (match.Success)
+            {
+                var span = int.Parse(match.Groups["span"].Value);
+                var unit = match.Groups["unit"].Value[0];
+                Func<double, TimeSpan> creator = unit switch
+                {
+                    'd' => TimeSpan.FromDays,
+                    'h' => TimeSpan.FromHours,
+                    'm' => x => TimeSpan.FromDays(x * 30),
+                    'y' => x => TimeSpan.FromDays(365 * x),
+                    var c => throw new InvalidOperationException($"Unknown specifier, {c}")
+                };
+                ts = -creator(span);
+                return true;
+            }
+            return false;
         }
     }
 }
--- test/OldPartitionsRemover.UnitTests/ByDateRemoving/ArgumentsTests.cs
+++ test/OldPartitionsRemover.UnitTests/ByDateRemoving/ArgumentsTests.cs
@@ -47,6 +47,51 @@ public class ArgumentsTests
     }
 
     [Test]
+    public void GetThreshold_WithMinusHourString_ReturnsThresholdWithinPreviousTwoHours()
+    {
+        var arguments = new Arguments
+        {
+            ThresholdString = "-1h"
+        };
+
+        var threshold = arguments.GetThreshold();
+
+        threshold.IsOk(out var d, out var _).Should().BeTrue();
+        d.Should().BeBefore(DateTimeOffset.Now);
+        d.Should().BeAfter(DateTimeOffset.Now - TimeSpan.FromHours(2));
+    }
+
+    [Test]
+    public void GetThreshold_WithMinusMonthString_ReturnsThresholdWithin10Days()
+    {
+        var arguments = new Arguments
+        {
+            ThresholdString = "-1m"
+        };
+
+        var threshold = arguments.GetThreshold();
+
+        threshold.IsOk(out var d, out var _).Should().BeTrue();
+        d.Should().BeBefore(DateTimeOffset.Now - TimeSpan.FromDays(20));
+        d.Should().BeAfter(DateTimeOffset.Now - TimeSpan.FromDays(40));
+    }
+
+    [Test]
+    public void GetThreshold_WithMinusYearString_ReturnsThresholdWithinHundredDays()
+    {
+        var arguments = new Arguments
+        {
+            ThresholdString = "-1y"
+        };
+
+        var threshold = arguments.GetThreshold();
+
+        threshold.IsOk(out var d, out var _).Should().BeTrue();
+        d.Should().BeBefore(DateTimeOffset.Now - TimeSpan.FromDays(300));
+        d.Should().BeAfter(DateTimeOffset.Now - TimeSpan.FromDays(400));
+    }
+
+    [Test]
     public void GetThreshold_WithDateString_ReturnsExactThreshold()
     {
         var arguments = new Arguments
