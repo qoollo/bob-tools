ca8af48337a99ed2f72d03e93624395a7efead99
Ivan Druzhitskiy <idruzhitskiy@mail.ru>
2022-02-15 18:01:17 +0300

    update configuration finder interface

--- src/BobToolsCli/ConfigurationFinding/IConfigurationFinder.cs
+++ src/BobToolsCli/ConfigurationFinding/IConfigurationFinder.cs
@@ -1,11 +1,12 @@
 using System.Threading;
 using System.Threading.Tasks;
 using BobApi.BobEntities;
+using BobToolsCli.ConfigurationReading;
 
 namespace BobToolsCli.ConfigurationFinding
 {
     public interface IConfigurationFinder
     {
-        Task<YamlReadingResult<ClusterConfiguration>> FindClusterConfiguration(CancellationToken cancellationToken = default);
+        Task<ConfigurationReadingResult<ClusterConfiguration>> FindClusterConfiguration(CancellationToken cancellationToken = default);
     }
 }
