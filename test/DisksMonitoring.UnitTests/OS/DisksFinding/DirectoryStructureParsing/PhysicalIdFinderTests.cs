5ec8253c13834be05a33fe035e53a9699ba3874b
Ivan Druzhitskiy <idruzhitskiy@mail.ru>
2022-05-11 16:47:52 +0300

    add test project

---
+++ test/DisksMonitoring.UnitTests/OS/DisksFinding/DirectoryStructureParsing/PhysicalIdFinderTests.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+using AutoFixture;
+using AutoFixture.AutoFakeItEasy;
+using AutoFixture.NUnit3;
+using DisksMonitoring.OS.DisksFinding.DirectoryStructureParsing;
+using FakeItEasy;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace DisksMonitoring.UnitTests.OS.DisksFinding.DirectoryStructureParsing
+{
+    public class PhysicalIdFinderTests
+    {
+        Test, AD]
+        public void Find_WithEmptyDir_ReturnsEmptyDictionary(
+            IFileSystemAccessor fileSystemAccessor,
+            PhysicalIdFinder sut)
+        {
+            A.CallTo(() => fileSystemAccessor.GetFilenames("/dev/disk/by-path"))
+                .Returns(new List<string>());
+
+            var result = sut.Find();
+
+            result.Should().BeEmpty();
+        }
+
+        private class ADAttribute : AutoDataAttribute
+        {
+            public ADAttribute() : base(() => new Fixture().Customize(new AutoFakeItEasyCustomization())) { }
+        }
+    }
+}
