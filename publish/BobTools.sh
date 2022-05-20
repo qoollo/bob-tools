37c5bf6fe967d0835a3c17c80aca904ea55bb6a5
Ivan Druzhitskiy <idruzhitskiy@mail.ru>
2022-05-20 15:47:24 +0300

    add support for better help

--- publish/BobTools.sh
+++ publish/BobTools.sh
@@ -1,23 +1,37 @@
 #!/bin/bash
 
-printHelp() {
+function printHelp {
     tools=(
-        BobAliensRecovery
-        BobClusterModifier
-        BobDisksMonitoring
-        BobRecordsCalculator)
-    help_BobAliensRecovery="Recover aliens in bob cluster"
-    help_BobClusterModifier="Expand bob cluster, moving data as needed"
-    help_BobDisksMonitoring="Monitor and reconnect disks for single bob node"
-    help_BobRecordsCalculator="Count records in bob cluster"
+        AliensRecovery
+        ClusterModifier
+        DisksMonitoring
+        RecordsCalculator)
+    help_AliensRecovery="Recover aliens in bob cluster"
+    help_ClusterModifier="Expand bob cluster, moving data as needed"
+    help_DisksMonitoring="Monitor and reconnect disks for single bob node"
+    help_RecordsCalculator="Count records in bob cluster"
     echo "Available tools:"
     for item in ${tools[*]}
     do
-        test="help_${item}"
-        echo -e "\t$item\t${!test}"
+        text="help_${item}"
+        names=($item Bob$item)
+        for name in ${names[*]}
+        do
+            ([[ -x "$(command -v ${name})" ]] || [[ -f $name ]]) && printf "\t%-20s %s\n\n" "$item" "${!text}"
+        done
     done
 }
 
-[[ $1 == --help ]] && printHelp && exit
-echo continue
+[[ $# == 0 ]] && echo "At least one argument is required, use --help to get available tools" && exit
+[[ $1 == --help ]] && [[ $# == 1 ]] && printHelp && exit
+
+names=($1 Bob$1 ${1/Bob/})
+command=${@:2}
+[[ $1 == --help ]] && [[ $# == 2 ]] && names=($2 Bob$2 ${2/Bob/}) && command=--help
+
+for item in ${names[*]}
+do
+    [[ -x "$(command -v $item)" ]] && $item $command && exit
+    [[ -f "$item" ]] && ./$item $command && exit
+done
 # ./$1 ${@:2}
