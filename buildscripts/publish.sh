756da4cd65cf6c172f98bb43d778bccfb4b7536e
Ivan Druzhitskiy <idruzhitskiy@mail.ru>
2022-05-25 14:53:09 +0300

    update runtime var

--- buildscripts/publish.sh
+++ buildscripts/publish.sh
@@ -2,7 +2,7 @@
 VERSION=${1:-$(git describe --tags | sed -r 's/v([0-9].[0-9].[0-9]).*/\1/g')}
 INFORMATIONALVERSION=${2:-$(git describe --tags)}
 CONFIGURATION=Release
-RUNTIME=${3-linux-x64}
+RUNTIME=${3:-linux-x64}
 
 echo $VERSION
 dotnet publish -c $CONFIGURATION -r $RUNTIME -p:Version=$VERSION -p:InformationalVersion=$INFORMATIONALVERSION -o publish src/DisksMonitoring
