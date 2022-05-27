f582014418c9d30195e2bedb4093e09d9b98447e
Ivan Druzhitskiy <idruzhitskiy@mail.ru>
2022-05-27 15:28:55 +0300

    move version to props file

--- buildscripts/publish.sh
+++ buildscripts/publish.sh
@@ -1,13 +1,11 @@
 #! /bin/sh
-VERSION=${1:-$(git describe --tags | sed -r 's/v([0-9].[0-9].[0-9]).*/\1/g')}
-INFORMATIONALVERSION=${2:-$(git describe --tags)}
 CONFIGURATION=Release
-RUNTIME=${3:-linux-x64}
+RUNTIME=${1:-linux-x64}
 
 echo $VERSION
-dotnet publish -c $CONFIGURATION -r $RUNTIME -p:Version=$VERSION -p:InformationalVersion=$INFORMATIONALVERSION -o publish src/DisksMonitoring
-dotnet publish -c $CONFIGURATION -r $RUNTIME -p:Version=$VERSION -p:InformationalVersion=$INFORMATIONALVERSION -o publish src/OldPartitionsRemover
-dotnet publish -c $CONFIGURATION -r $RUNTIME -p:Version=$VERSION -p:InformationalVersion=$INFORMATIONALVERSION -o publish src/RecordsCalculator
-dotnet publish -c $CONFIGURATION -r $RUNTIME -p:Version=$VERSION -p:InformationalVersion=$INFORMATIONALVERSION -o publish src/DiskStatusAnalyzer
-dotnet publish -c $CONFIGURATION -r $RUNTIME -p:Version=$VERSION -p:InformationalVersion=$INFORMATIONALVERSION -o publish src/ClusterModifier
-dotnet publish -c $CONFIGURATION -r $RUNTIME -p:Version=$VERSION -p:InformationalVersion=$INFORMATIONALVERSION -o publish src/BobAliensRecovery
+dotnet publish -c $CONFIGURATION -r $RUNTIME -o publish src/DisksMonitoring
+dotnet publish -c $CONFIGURATION -r $RUNTIME -o publish src/OldPartitionsRemover
+dotnet publish -c $CONFIGURATION -r $RUNTIME -o publish src/RecordsCalculator
+dotnet publish -c $CONFIGURATION -r $RUNTIME -o publish src/DiskStatusAnalyzer
+dotnet publish -c $CONFIGURATION -r $RUNTIME -o publish src/ClusterModifier
+dotnet publish -c $CONFIGURATION -r $RUNTIME -o publish src/BobAliensRecovery
