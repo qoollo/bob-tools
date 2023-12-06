42fe8a3a2c17595009c0b8de04c8bb4757b03ad0
Ivan Druzhitskiy <idruzhitskiy@gmail.com>
2023-12-06 18:11:42 +0300

    refactor disks monitoring cli to use common bobtoolscli

---
+++ src/DisksMonitoring/ConfigurationGenerator.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using BobApi;
+using BobToolsCli.Exceptions;
+using DisksMonitoring.Config;
+using Microsoft.Extensions.Logging;
+
+namespace DisksMonitoring;
+
+class ConfigurationGenerator
+{
+    private readonly Configuration _configuration;
+    private readonly ILogger<ConfigurationGenerator> _logger;
+
+    public ConfigurationGenerator(
+        Configuration configuration,
+        ILogger<ConfigurationGenerator> logger
+    )
+    {
+        _configuration = configuration;
+        _logger = logger;
+    }
+
+    public async Task<Configuration> Generate(
+        string stateFile,
+        BobApiClient bobApiClient,
+        CancellationToken cancellationToken
+    )
+    {
+        _logger.LogInformation("Reading configuration...");
+        try
+        {
+            await _configuration.ReadFromFile(stateFile);
+            await _configuration.AddEntriesFromBob(bobApiClient);
+            await _configuration.SaveToFile(stateFile);
+            return _configuration;
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(
+                $"Exception while getting info from config: {e.Message}{Environment.NewLine}{e.StackTrace}"
+            );
+            throw;
+        }
+        finally
+        {
+            _logger.LogInformation("Reading configuration done");
+        }
+    }
+}
---
+++ src/DisksMonitoring/Monitor.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+using DisksMonitoring.Bob;
+using DisksMonitoring.Config;
+using DisksMonitoring.OS;
+using Microsoft.Extensions.Logging;
+
+namespace DisksMonitoring;
+
+class Monitor
+{
+    private readonly MonitorArguments _args;
+    private readonly DisksMonitor _disksMonitor;
+    private readonly ExternalScriptsRunner _externalScriptsRunner;
+    private readonly DisksStarter _disksStarter;
+    private readonly ILogger<Monitor> _logger;
+
+    public Monitor(
+        MonitorArguments args,
+        DisksMonitor disksMonitor,
+        ExternalScriptsRunner externalScriptsRunner,
+        DisksStarter disksStarter,
+        ILogger<Monitor> logger
+    )
+    {
+        _args = args;
+        _disksMonitor = disksMonitor;
+        _externalScriptsRunner = externalScriptsRunner;
+        _disksStarter = disksStarter;
+        _logger = logger;
+    }
+
+    public async Task Run(Configuration configuration, CancellationToken cancellationToken)
+    {
+        var span = TimeSpan.FromSeconds(configuration.MinCycleTimeSec);
+        var client = await _args.GetLocalBobClient(cancellationToken);
+        _logger.LogInformation("Start monitor");
+        while (true)
+        {
+            var sw = Stopwatch.StartNew();
+            try
+            {
+                await _externalScriptsRunner.RunPreCycleScripts();
+                await _disksMonitor.CheckAndUpdate(client);
+                await configuration.SaveToFile(_args.StateFile);
+                await _disksStarter.StartDisks(client);
+                await _externalScriptsRunner.RunPostCycleScripts();
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(
+                    $"Exception while processing cycle: {e.Message}{Environment.NewLine}{e.StackTrace}"
+                );
+            }
+            sw.Stop();
+            if (sw.Elapsed < span)
+                await Task.Delay(span - sw.Elapsed);
+        }
+    }
+}
