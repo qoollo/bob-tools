f6e70f2791733df1c12560b74265a6617489b746
idruzhitskiy <idruzhitskiy@mail.ru>
2023-12-19 16:16:45 +0300

    ClusterModifier: 94 fix local delete (#95)
    * fix local delete
    
    * reuse remove empty subdirs
    
    * fix method invoke
    
    * invoke remove directly
    
    * fix typo

--- src/RemoteFileCopy/LocalOptimizedRemoteFileCopier.cs
+++ src/RemoteFileCopy/LocalOptimizedRemoteFileCopier.cs
@@ -42,10 +42,9 @@ namespace RemoteFileCopy
             {
                 if (!Directory.Exists(path))
                     return false;
-                foreach (var f in Directory.GetFiles(path))
+                foreach (var f in Directory.EnumerateFiles(path, "*", SearchOption.AllDirectories))
                     File.Delete(f);
-                foreach (var d in Directory.GetDirectories(path))
-                    Directory.Delete(d);
+                ClearAndCheckIsEmpty(path);
                 return true;
             }
             return await _remoteFileCopier.RemoveInDir(dir, cancellationToken);
