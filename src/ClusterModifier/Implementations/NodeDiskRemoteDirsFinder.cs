c22aee1adfc9a6458cd7d7b8870f20b7011c81a0
Ivan Druzhitskiy <idruzhitskiy@gmail.com>
2024-01-19 12:23:37 +0300

    ClusterModifier: fetch alien dirs from client

--- src/ClusterModifier/Implementations/NodeDiskRemoteDirsFinder.cs
+++ src/ClusterModifier/Implementations/NodeDiskRemoteDirsFinder.cs
@@ -3,13 +3,14 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using BobApi.BobEntities;
+using BobToolsCli.Exceptions;
+using BobToolsCli.Helpers;
 using RemoteFileCopy.Entities;
 
 namespace ClusterModifier;
 
 public class NodeDiskRemoteDirsFinder : INodeDiskRemoteDirsFinder
 {
-    private const string AlienDir = "alien";
     private readonly ClusterExpandArguments _args;
 
     public NodeDiskRemoteDirsFinder(ClusterExpandArguments args)
@@ -25,16 +26,37 @@ public class NodeDiskRemoteDirsFinder : INodeDiskRemoteDirsFinder
     )
     {
         var result = new Dictionary<string, Dictionary<string, RemoteDir>>();
+        var prov = _args.GetBobApiClientProvider();
         foreach (var node in config.Nodes)
         {
-            var remoteDirByDisk = await GetRemoteDirByDisk(node, cancellationToken);
-            result.Add(node.Name, remoteDirByDisk);
+            var dir = await FindAlienDir(prov, node, cancellationToken);
+            // There is a possibility for multiple disks in future
+            result.Add(node.Name, new Dictionary<string, RemoteDir> { ["alien"] = dir });
         }
 
-        return result.ToDictionary(
-            kv => kv.Key,
-            kv => kv.Value.ToDictionary(kv1 => kv1.Key, kv1 => kv1.Value.GetSubdir(AlienDir))
-        );
+        return result;
+    }
+
+    private static async Task<RemoteDir> FindAlienDir(
+        BobApiClientProvider prov,
+        ClusterConfiguration.Node node,
+        CancellationToken cancellationToken
+    )
+    {
+        var client = prov.GetClient(node);
+        var alienDir = await client.GetAlienDirectory(cancellationToken);
+        RemoteDir dir;
+        if (alienDir.IsOk(out var d, out var err))
+        {
+            var addr = await node.FindIPAddress();
+            dir = new RemoteDir(addr, d.Path);
+        }
+        else
+        {
+            throw new ClusterStateException($"Failed to get alien dir for node {node.Name}: {err}");
+        }
+
+        return dir;
     }
 
     public async Task<
